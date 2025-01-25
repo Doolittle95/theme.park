ae130ad5e7c8c48ec8868c4bbbdee0836b954b00
Doolittle95 <Doolittle95@outlook.com>
2025-01-25 19:12:53 +0000

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -7,24 +7,11 @@
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre>
-                                                                                                                                                
-DDDDDDDDDDDDD                                          lllllll   iiii           tttt                   tttt          lllllll                     
-D::::::::::::DDD                                       l:::::l  i::::i       ttt:::t                ttt:::t          l:::::l                     
-D:::::::::::::::DD                                     l:::::l   iiii        t:::::t                t:::::t          l:::::l                     
-DDD:::::DDDDD:::::D                                    l:::::l               t:::::t                t:::::t          l:::::l                     
-  D:::::D    D:::::D    ooooooooooo      ooooooooooo    l::::l iiiiiii ttttttt:::::ttttttt    ttttttt:::::ttttttt     l::::l     eeeeeeeeeeee    
-  D:::::D     D:::::D oo:::::::::::oo  oo:::::::::::oo  l::::l i:::::i t:::::::::::::::::t    t:::::::::::::::::t     l::::l   ee::::::::::::ee  
-  D:::::D     D:::::Do:::::::::::::::oo:::::::::::::::o l::::l  i::::i t:::::::::::::::::t    t:::::::::::::::::t     l::::l  e::::::eeeee:::::ee
-  D:::::D     D:::::Do:::::ooooo:::::oo:::::ooooo:::::o l::::l  i::::i tttttt:::::::tttttt    tttttt:::::::tttttt     l::::l e::::::e     e:::::e
-  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e:::::::eeeee::::::e
-  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e:::::::::::::::::e 
-  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e::::::eeeeeeeeeee  
-  D:::::D    D:::::D o::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t    tttttt      t:::::t    tttttt l::::l e:::::::e           
-DDD:::::DDDDD:::::D  o:::::ooooo:::::oo:::::ooooo:::::ol::::::li::::::i      t::::::tttt:::::t      t::::::tttt:::::tl::::::le::::::::e          
-D:::::::::::::::DD   o:::::::::::::::oo:::::::::::::::ol::::::li::::::i      tt::::::::::::::t      tt::::::::::::::tl::::::l e::::::::eeeeeeee  
-D::::::::::::DDD      oo:::::::::::oo  oo:::::::::::oo l::::::li::::::i        tt:::::::::::tt        tt:::::::::::ttl::::::l  ee:::::::::::::e  
-DDDDDDDDDDDDD           ooooooooooo      ooooooooooo   lllllllliiiiiiii          ttttttttttt            ttttttttttt  llllllll    eeeeeeeeeeeeee  
-                                                                                                                                                 
-  </pre> </div>
+  ___                _   _   _     _     _       
+ |   \   ___   ___  | | (_) | |_  | |_  | |  ___ 
+ | |) | / _ \ / _ \ | | | | |  _| |  _| | | / -_)
+ |___/  \___/ \___/ |_| |_|  \__|  \__| |_| \___|
+                                                 
+</pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
