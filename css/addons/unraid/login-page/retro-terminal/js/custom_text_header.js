8ea56c06f91f44bbcba41b2933a949908242e08a
Doolittle95 <Doolittle95@outlook.com>
2025-01-25 19:09:07 +0000

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,25 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
-                                                                 </pre> </div>
+<div class="custom-text-header"> <pre>
+                                                                                                                                                
+DDDDDDDDDDDDD                                          lllllll   iiii           tttt                   tttt          lllllll                     
+D::::::::::::DDD                                       l:::::l  i::::i       ttt:::t                ttt:::t          l:::::l                     
+D:::::::::::::::DD                                     l:::::l   iiii        t:::::t                t:::::t          l:::::l                     
+DDD:::::DDDDD:::::D                                    l:::::l               t:::::t                t:::::t          l:::::l                     
+  D:::::D    D:::::D    ooooooooooo      ooooooooooo    l::::l iiiiiii ttttttt:::::ttttttt    ttttttt:::::ttttttt     l::::l     eeeeeeeeeeee    
+  D:::::D     D:::::D oo:::::::::::oo  oo:::::::::::oo  l::::l i:::::i t:::::::::::::::::t    t:::::::::::::::::t     l::::l   ee::::::::::::ee  
+  D:::::D     D:::::Do:::::::::::::::oo:::::::::::::::o l::::l  i::::i t:::::::::::::::::t    t:::::::::::::::::t     l::::l  e::::::eeeee:::::ee
+  D:::::D     D:::::Do:::::ooooo:::::oo:::::ooooo:::::o l::::l  i::::i tttttt:::::::tttttt    tttttt:::::::tttttt     l::::l e::::::e     e:::::e
+  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e:::::::eeeee::::::e
+  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e:::::::::::::::::e 
+  D:::::D     D:::::Do::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t                t:::::t           l::::l e::::::eeeeeeeeeee  
+  D:::::D    D:::::D o::::o     o::::oo::::o     o::::o l::::l  i::::i       t:::::t    tttttt      t:::::t    tttttt l::::l e:::::::e           
+DDD:::::DDDDD:::::D  o:::::ooooo:::::oo:::::ooooo:::::ol::::::li::::::i      t::::::tttt:::::t      t::::::tttt:::::tl::::::le::::::::e          
+D:::::::::::::::DD   o:::::::::::::::oo:::::::::::::::ol::::::li::::::i      tt::::::::::::::t      tt::::::::::::::tl::::::l e::::::::eeeeeeee  
+D::::::::::::DDD      oo:::::::::::oo  oo:::::::::::oo l::::::li::::::i        tt:::::::::::tt        tt:::::::::::ttl::::::l  ee:::::::::::::e  
+DDDDDDDDDDDDD           ooooooooooo      ooooooooooo   lllllllliiiiiiii          ttttttttttt            ttttttttttt  llllllll    eeeeeeeeeeeeee  
+                                                                                                                                                 
+  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
