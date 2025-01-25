c448e3a964172f824c074f486cb4b926ac102aa0
Doolittle95 <Doolittle95@outlook.com>
2025-01-25 20:57:48 +0000

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,13 +6,11 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
- _    _        _                               
+<div class="custom-text-header"> <pre> _    _        _                               
 | |  | |      | |                              
 | |  | |  ___ | |  ___   ___   _ __ ___    ___ 
 | |/\| | / _ \| | / __| / _ \ | '_ ` _ \  / _ \
 \  /\  /|  __/| || (__ | (_) || | | | | ||  __/
- \/  \/  \___||_| \___| \___/ |_| |_| |_| \___|
-                                               </pre> </div>
+ \/  \/  \___||_| \___| \___/ |_| |_| |_| \___|</pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
